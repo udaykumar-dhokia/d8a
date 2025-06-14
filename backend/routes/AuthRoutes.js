3c7af6d889cbf9db2515fdad42fdb425002d118a
udaykumar-dhokia <udaykumardhokia@gmail.com>
2025-06-14 23:55:09 +0530

    Fix: Session Token Error

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -4,6 +4,6 @@ import AuthController from "../controllers/AuthController.js";
 
 router.post("/login", AuthController.loginUser);
 router.post("/register", AuthController.registerUser);
-router.post("/verify-token", AuthController.checkToken);
+router.post("/verify-token", AuthController.verifyToken);
 
 export default router;
